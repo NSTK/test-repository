d76e1a16006d13b1e9c21e0b3fbaf46d250ffecd
Nikita <nikita.skripka@gmail.com>
2024-12-15 18:40:40 +0300

    Update App.js text

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Скрипка Н.А.";
+  const greeting = "Это проект Скрипка Н.А.";
 
-   return (
+  return (
     <div className="App">
-     <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         {greeting}
+        {greeting}
       </header>
     </div>
   );
